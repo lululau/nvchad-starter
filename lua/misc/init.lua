efead555f240734c1226b327566da59588e0bd6d
Liu Xiang <liuxiang@upeast.com.cn>
2024-03-29 22:08:56 +0800

    Add mapping for telescope project

--- lua/misc/init.lua
+++ lua/misc/init.lua
@@ -9,6 +9,8 @@ vim.api.nvim_set_keymap("i", "<M-Return>", "<ESC>:Copilot panel<CR>", { noremap 
 vim.api.nvim_set_keymap("n", "<leader>pp", "<cmd> Telescope projects<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<D-p>", "<cmd> Telescope projects<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<D-p>", "<cmd> Telescope projects<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-x>@sp", "<cmd> Telescope projects<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-x>@sp", "<cmd> Telescope projects<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "gp", "`[V`]", { noremap = true, silent = true })
 
